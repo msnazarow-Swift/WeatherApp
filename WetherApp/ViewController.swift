e6f7c5ee79e0527d0e29902ba24a6ca0e2e3d1cb
Mihail Nazaro <msnazarow@gmail.com>
2021-09-03 00:41:28 +0300

    Initial Commit

---
+++ WetherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WetherApp
+//
+//  Created by out-nazarov2-ms on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
